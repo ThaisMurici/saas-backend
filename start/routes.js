2714f9a6473cdf3a897d79f25a56defcb4c521d8
Thais Moraes <murici.thais@gmail.com>
2019-05-09 19:32:14 -0300

    Add Team CRUD

--- start/routes.js
+++ start/routes.js
@@ -16,3 +16,7 @@
 const Route = use('Route')
 
 Route.post('sessions', 'SessionController.store')
+
+Route.group(() => {
+  Route.resource('teams', 'TeamController').apiOnly()
+}).middleware('auth')
